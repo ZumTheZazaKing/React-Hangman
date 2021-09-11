0fb543ab14a55f1d2731be3c127eb8e8bab947d1
ZumTheZazaKing <zahidi85543@gmail.com>
2021-09-11 16:32:08 +0800

    Shouldn't use redux

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
+import { Suspense, lazy } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+const Main = lazy(() => import('./Main').then(module => ({default:module.Main})));
+const Settings = lazy(() => import('./Settings').then(module => ({default:module.Settings})));
+const Ingame = lazy(() => import('./Ingame').then(module => ({default:module.Ingame})));
+
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Suspense fallback={<h2>Loading...</h2>}>
+          <Switch>
+
+            <Route exact path="/">
+              <Main/>
+            </Route>
+            <Route exact path="/ingame">
+              <Ingame/>
+            </Route>
+            <Route exact path="/settings">
+              <Settings/>
+            </Route>
+
+          </Switch>
+        </Suspense>
+      </div>
+    </Router>
   );
 }
 
