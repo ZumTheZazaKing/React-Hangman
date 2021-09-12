73b78d0e140952db87a50564ba8c7c72eebe2268
ZumTheZazaKing <zahidi85543@gmail.com>
2021-09-12 21:50:59 +0800

    Loading for ingame added

--- src/Main.js
+++ src/Main.js
@@ -1,15 +1,18 @@
 import Button from '@material-ui/core/Button';
 import { useHistory } from "react-router-dom";
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 
 import { Context } from './Contexts/Context';
 
 export function Main(){
 
     const history = useHistory();
+    const [buttonState, setButtonState] = useState(false);
     const { setWord, setWordHint } = useContext(Context);
 
     function generateWord(){
+
+        setButtonState(true);
 
         fetch('https://random-word-api.herokuapp.com/word?number=1')
         .then(res => res.json())
@@ -20,7 +23,8 @@ export function Main(){
             .then(res => res.json())
             .then(dataset => {
                 setWordHint(dataset[0].meanings[0].definitions[0].definition);
-                history.push("/ingame")
+                history.push("/ingame");
+                setButtonState(false);
             })
             .catch(()=> generateWord())
         });
@@ -29,6 +33,6 @@ export function Main(){
 
     return (<div id="Main">
         <h1>Hangman</h1>
-        <Button variant="contained" color="primary" onClick={() => generateWord()}>Play</Button>
+        <Button variant="contained" color="primary" disabled={buttonState} onClick={(() => generateWord())}>Play</Button>
     </div>)
 }
